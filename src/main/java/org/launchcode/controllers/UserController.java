129ee0827fcd22c00e1a61f673387fb88d28fc54
maxwell.maier <maxwellmaier89@gmail.com>
2024-05-13 21:53:15 -0500

    finished studio with Brett

--- src/main/java/org/launchcode/controllers/UserController.java
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -1,34 +1,38 @@
 package org.launchcode.controllers;
 
+import jakarta.validation.Valid;
 import org.launchcode.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+
 
 @Controller
 @RequestMapping("user")
 public class UserController {
 
     @GetMapping("/add")
-    public String displayAddUserForm() {
+    public String displayAddUserForm(Model model) {
+
+        model.addAttribute(new User());
         return "user/add";
     }
 
     @PostMapping
-    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+    public String processAddUserForm(Model model, @ModelAttribute @Valid User user, Errors errors, String verify) {
         model.addAttribute("user", user);
-        model.addAttribute("verify", verify);
-        model.addAttribute("username", user.getUsername());
-        model.addAttribute("email", user.getEmail());
-        if (user.getPassword().equals(verify)) {
+//        model.addAttribute("verify", verify);
+//        model.addAttribute("username", user.getUsername());
+//        model.addAttribute("email", user.getEmail());
+        if (!errors.hasErrors() && user.getPassword().equals(verify)) {
             return "user/index";
-        }
-        else {
+        } else {
+//            model.addAttribute("errors", errors);
             model.addAttribute("error", "Passwords do not match");
             return "user/add";
         }
 
+
     }
-
-
 }
--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,19 +1,38 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.*;
+
 public class User {
+    @NotBlank(message = "Username Required")
+    @Size(min = 5, max = 15)
     private String username;
+    @Email(message = "Must be a valid Email")
     private String email;
+    @NotBlank(message = "Password Required")
+    @Size(min = 6, message = "Password must be at least 6 characters long.")
     private String password;
+    private String verify;
+    @NotBlank(message = "Password Required")
+    @Size(min = 6, message = "Password must be at least 6 characters long.")
+    public String getVerify() {
+        return verify;
+    }
+
+    public void setVerify(String verify) {
+        this.verify = verify;
+        checkPassword();
+    }
 
     public User() {
 
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password, String verify) {
         this();
         this.username = username;
         this.email = email;
         this.password = password;
+        this.verify = verify;
     }
 
     public String getUsername() {
@@ -39,5 +58,11 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
-}
 
+
+    private void checkPassword () {
+        if (password != null && verify != null && !password.equals(verify)) {
+            setPassword(null);
+        }
+    }
+}
